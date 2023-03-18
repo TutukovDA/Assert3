ce6c98393e1a5e7abd8dcfdcec9548a7e9a9aa7f
TutukovDA <126241028+TutukovDA@users.noreply.github.com>
2023-03-18 15:11:44 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Получается "+ sum(10,5));
+        System.out.println("Получается "+ sum(10,5)+ "   "+sum(10,5));
+    }
+    public static int sum (int a, int b) {
+        return a+b;
+    }
+}
